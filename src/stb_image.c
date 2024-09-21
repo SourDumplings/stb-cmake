62b697fa134aa8e08bc43d5298374239e44901e7
SourDumplings <changzheng300@foxmail.com>
2024-09-21 20:26:55 +0800

    cmake fix, dir struct simplify

--- src/stb_image.c
+++ src/stb_image.c
@@ -1,3 +1,3 @@
 // Create stb image implementation
 #define STB_IMAGE_IMPLEMENTATION
-#include "../include/stb/stb_image.h"
+#include "stb_image.h"
--- src/stb_truetype.c
+++ src/stb_truetype.c
@@ -1,3 +1,3 @@
 // Create stb truetype implementation
 #define STB_TRUETYPE_IMPLEMENTATION
-#include "../include/stb/stb_truetype.h"
+#include "stb_truetype.h"
